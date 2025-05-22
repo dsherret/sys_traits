a36733f4e2caef649e7309190f54cc6af0e9eb88
tuhana <tuhana.cat+git@gmail.com>
2025-05-23 01:54:23 +0300

    fix: WASM test for `env_programs_dir`
    Expects `Some` only when the platform is Windows

--- tests/wasm_test/src/lib.rs
+++ tests/wasm_test/src/lib.rs
@@ -207,7 +207,10 @@ fn run(is_windows: bool) -> std::io::Result<()> {
 
   // just ensure these don't panic
   assert!(sys.env_home_dir().is_some());
-  assert!(sys.env_programs_dir().is_some());
+  assert!(matches!(
+    sys.env_programs_dir(),
+    Some(_) if is_windows
+  ));
   assert!(sys.env_cache_dir().is_some());
   assert!(sys.env_temp_dir().is_ok());
 
