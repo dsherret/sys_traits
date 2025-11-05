786b84997834e76a13b05e55baf1fded4b991ad3
David Sherret <dsherret@gmail.com>
2025-11-05 14:30:51 +0100

    update

--- tests/wasm_test/run.js
+++ tests/wasm_test/run.js
@@ -1,5 +1,5 @@
 // run `deno task wasmbuild` to generate this file
-import { instantiate } from "./lib/sys_traits_wasm_test.generated.js";
+import { instantiate } from "./lib/sys_traits_wasm_test.js";
 
 const { run_tests } = await instantiate();
 run_tests();
